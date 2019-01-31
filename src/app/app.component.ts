6d98a49a93b0b876a704177a8a7fb3f6cb4672f1
Rodriguez Elorza <elorzafe@amazon.com>
2019-01-31 13:05:16 -0800

    auth angular test refresh token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { AmplifyService } from 'aws-amplify-angular';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'authAngularTest';
+  amplifyService = null;
+  constructor( public amplify:AmplifyService) {
+    this.amplifyService = amplify;
+  }
+
+  login(){
+    console.log('login')
+    this.amplifyService.auth().signIn("xxxxxxx", "xxxxxxxxxx")
+  }
+  currentUser(){
+    console.log('currentUser')
+    this.amplifyService.auth().currentAuthenticatedUser().then(console.log)
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,19 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Amplify from 'aws-amplify';
+
+Amplify.configure({
+  Auth: {
+    identityPoolId: "config.aws_cognito_identity_pool_id",
+    region: "config.aws_project_region",
+    identityPoolRegion: "config.aws_cognito_region",
+    userPoolId: "config.aws_user_pools_id",
+    userPoolWebClientId: "config.aws_user_pools_web_client_id",
+    authenticationFlowType: 'USER_PASSWORD_AUTH'
+  },
+});
+
 if (environment.production) {
   enableProdMode();
 }
